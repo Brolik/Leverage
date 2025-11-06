877338ce6f517f934dc5ae0eb2e09c70856fb816
jassok <jassok@brolik.com>
2025-11-06 08:42:07 -0800

    deprecation fix for global libraries

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ const debug = require('gulp-debug');
 const flatten = require('gulp-flatten');
 const postcss = require('gulp-postcss');
 const sass = require('gulp-dart-sass');
+// Use the official Dart Sass JS implementation as the compiler to avoid
+// legacy JS API usage. This keeps `gulp-dart-sass` while ensuring it calls
+// into the modern `sass` package.
+const dartSass = require('sass');
+sass.compiler = dartSass;
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
 
@@ -14,7 +19,6 @@ const imageGlob = [
 ];
 
 const sassGlob = [
-  './src/components/**/*.{sass,scss}',
   './examples/**/*.{sass,scss}',
   './sass/**/*.{sass,scss}'
 ];
@@ -22,11 +26,6 @@ const sassGlob = [
 const globalJsGlob = [];
 
 const site = {
-  images: () => {
-    return src(imageGlob)
-      .pipe(flatten())
-    .pipe(dest(`${outputPath}/images`))
-  },
   sass: () => {
     return src(sassGlob)
       .pipe(debug({ title: 'file:' }))
@@ -46,9 +45,8 @@ const site = {
     .pipe(dest(`${outputPath}/css`))
   },
   watcher: () => {
-    watch(imageGlob, series(site.images));
     watch(sassGlob, series(site.sass));
   }
 }
 
-exports.default = series(parallel(site.images, site.sass), site.watcher);
+exports.default = series(parallel(site.sass), site.watcher);
